21a48c6638b6adf1fdc4cabc4472047d02f61222
Abdul Latif Mubasir <bj.angel119@gmail.com>
2021-07-25 13:15:45 +0700

    lesson 14 - matching a username

--- validation.js
+++ validation.js
@@ -1,7 +1,10 @@
 // validation script here
 const inputs = document.querySelectorAll('input');
 const patterns = {
-    telephone: /^\d{11}$/
+    telephone: /^\d{11}$/,
+    username: /^[a-z\d]{5,12}$/i,
+    password: /^[\w@-]{8,20}$/,
+    slug: /^[a-z\d-]{8,20}$/,
 }
 
 // validation function
@@ -9,7 +12,7 @@ function validate(field , regex) {
     if(regex.test(field.value)) {
         field.className = 'valid';
     } else {
-        field.className = 'invalid';
+        field.className = 'invalid'; 
     }
 }
 
